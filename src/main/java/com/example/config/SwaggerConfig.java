1c82545b6be790e20c64cc9b40255b328621ec10
LeeJunGyun <rainmaker0303@hanmail.net>
2023-07-14 22:02:49 +0900

    swagger 3.0 추가

---
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "API 명세서",
+                description = "API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        String[] paths = {"/v1/**", "/api/**/**", "**/api/**/**"};
+        return GroupedOpenApi.builder()
+                .group("API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
